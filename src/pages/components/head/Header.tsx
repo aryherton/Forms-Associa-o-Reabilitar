33dcba461105b9536fc05d83497df649c98b5022
@aryhertonbarbosa <aryhertonbarbosa@gmail.com>
2023-01-20 13:13:59 -0300

    fix: field "app"

--- src/pages/components/head/Header.tsx
+++ src/pages/components/head/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../../../appTest/globals.css';
+import '../../../app/globals.css';
 import Image from "next/image";
 
 import HeaderWrapper from './Header.style';
