306d6a9ec238e1f249c7b2c1375657a42f04f274
@aryhertonbarbosa <aryhertonbarbosa@gmail.com>
2023-04-18 08:03:06 -0300

    refactor: change white space in file "linkForm.ts"

--- src/utils/linkForm.ts
+++ src/utils/linkForm.ts
@@ -1,24 +1,24 @@
 const arrLinkForm = [
-    {
-        descricao: 'INFORMAÇÕES DIÁRIAS',
-        linkForm: 'https://forms.zohopublic.com/adilio/form/INFORMAESDIRIAS/formperma/qYBm16bKlJY6LgPkYvUlE120Pf2N8O3Au4V5_bNXwUk',
-        icon: '/listaInfo.png'
-    },
-    {
-        descricao: 'DIÁRIO DE BORDO',
-        linkForm: 'https://forms.zohopublic.com/adilio/form/DIRIODEBORDOteste/formperma/UihceI7Txe6nywTF4CpLD2mHqKgE4J0gWDR2jU2eXcE',
-        icon: '/diarioBordo.png'
-    },
-    {
-        descricao: 'MAPA COMPLEMENTAR',
-        linkForm: 'https://forms.zohopublic.com/heltonpqd/form/MAPACOMPLEMENTARSTE/formperma/Re4WwZAW12A5gNOcIzPc0u4p6UhgitWor4O0CdP4PVo',
-        icon: '/mapaComple.png'
-    },
-    {
-        descricao: 'SOLICITAÇÃO DE SERVIÇOS',
-        linkForm: 'https://forms.zohopublic.com/adilio/form/SOLICITAODESERVIOS/formperma/Fr_QFwOkECv8tlKOQzJEweehUMPC0Kdf7si-OfJGqMg',
-        icon: '/manutencao.png'
-    }
+  {
+    descricao: 'INFORMAÇÕES DIÁRIAS',
+    linkForm: 'https://forms.zohopublic.com/adilio/form/INFORMAESDIRIAS/formperma/qYBm16bKlJY6LgPkYvUlE120Pf2N8O3Au4V5_bNXwUk',
+    icon: '/listaInfo.png'
+  },
+  {
+    descricao: 'DIÁRIO DE BORDO',
+    linkForm: 'https://forms.zohopublic.com/adilio/form/DIRIODEBORDOteste/formperma/UihceI7Txe6nywTF4CpLD2mHqKgE4J0gWDR2jU2eXcE',
+    icon: '/diarioBordo.png'
+  },
+  {
+    descricao: 'MAPA COMPLEMENTAR',
+    linkForm: 'https://forms.zohopublic.com/heltonpqd/form/MAPACOMPLEMENTARSTE/formperma/Re4WwZAW12A5gNOcIzPc0u4p6UhgitWor4O0CdP4PVo',
+    icon: '/mapaComple.png'
+  },
+  {
+    descricao: 'SOLICITAÇÃO DE SERVIÇOS',
+    linkForm: 'https://forms.zohopublic.com/adilio/form/SOLICITAODESERVIOS/formperma/Fr_QFwOkECv8tlKOQzJEweehUMPC0Kdf7si-OfJGqMg',
+    icon: '/manutencao.png'
+  }
 ]
 
 export default arrLinkForm
