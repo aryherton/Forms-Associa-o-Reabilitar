f82c50d65899aae52068ca1d3b2ebfbed0ba7585
aryheton <aryhertonbarbosa@gmail.com>
2023-09-17 23:20:41 -0300

    fix: fixed erro layout. change path file footer.style.js

--- src/pages/components/footer/Footer.tsx
+++ src/pages/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-import FooterWrapper from '../../../styles/footer.style.js';
+import FooterWrapper from './footer.style';
 
 export default function Footer() {
   return (
